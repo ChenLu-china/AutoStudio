b4ee4006a802f5a0fa416ac624eb2b9231060e95
ChenLu-china <chen-lu.chen@connect.polyu.hk>
2023-09-14 18:46:19 +0800

    c++ dataset branch

---
+++ c++/src/data/dataset.cpp
@@ -0,0 +1,10 @@
+#include <torch/torch.h>
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
---
+++ c++/src/data/dataset.h
@@ -0,0 +1,24 @@
+
+#pragma once
+#include <torch/torch.h>
+
+using Tensor = torch::Tensor;
+
+
+class Dataset
+{
+private:
+    /* data */
+public:
+    Dataset(/* args */);
+    ~Dataset();
+};
+
+Dataset::Dataset(/* args */)
+{
+}
+
+Dataset::~Dataset()
+{
+}
+
